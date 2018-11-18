06ae4048234e0a6bdabed922799940d711f692ae
Norton Nice <nortonnice@gmail.com>
2018-11-18 09:47:36 -0800

    updated burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,29 @@
 var orm = require('../config/orm.js');
 
-orm.selectAll
+//NEED TO FIX THIS , PULLED FROM CATS.JS//////
+var burger = {
+        all: function(cb) {
+          orm.all("cats", function(res) {
+            cb(res);
+          });
+        },
+        // The variables cols and vals are arrays.
+        create: function(cols, vals, cb) {
+          orm.create("cats", cols, vals, function(res) {
+            cb(res);
+          });
+        },
+        update: function(objColVals, condition, cb) {
+          orm.update("cats", objColVals, condition, function(res) {
+            cb(res);
+          });
+        },
+        delete: function(condition, cb) {
+          orm.delete("cats", condition, function(res) {
+            cb(res);
+          });
+        }
+      };
 /*this is exprted to burgers_controller - look at hbs 12 for expample
 burger.js will point to burger_controller.js*/
 
